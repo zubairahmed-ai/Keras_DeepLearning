ef6dd44154447dcd17df12171990cb09fa549030
Zubair Ahmed <zubairdotnet@hotmail.com>
2017-12-03 14:16:49 +0500

    Prediction of all images in a folder
    car.jpg - Prediction: sports_car (91.77%)
    car2.jpg - Prediction: beach_wagon (54.96%)
    cat.jpg - Prediction: Egyptian_cat (34.26%)
    plane.jpg - Prediction: airliner (75.09%)

--- Model.py
+++ Model.py
@@ -3,21 +3,28 @@ from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
+import os
 # load the model
 model = VGG16()
-# load an image from file
-image = load_img('cat.jpg', target_size=(224, 224))
-# convert the image pixels to a numpy array
-image = img_to_array(image)
-# reshape data for the model
-image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-# prepare the image for the VGG model
-image = preprocess_input(image)
-# predict the probability across all output classes
-yhat = model.predict(image)
-# convert the probabilities to class labels
-label = decode_predictions(yhat)
-# retrieve the most likely result, e.g. highest probability
-label = label[0][0]
-# print the classification
-print('%s (%.2f%%)' % (label[1], label[2]*100))
+
+def predict(fileName):
+    # load an image from file
+    image = load_img('./images/' + fileName, target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # predict the probability across all output classes
+    yhat = model.predict(image)
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    # retrieve the most likely result, e.g. highest probability
+    label = label[0][0]
+    # print the classification
+    print('%s - Prediction: %s (%.2f%%)' % (fileName, label[1], label[2]*100))
+
+files = [f for f in os.listdir('./images') if f.endswith(".jpg")]
+for f in files:
+    predict(f)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 This code uses Keras to predict a cat image in the picture using the VGG 16 pre-train model
+
+Results:
+    car.jpg - Prediction: sports_car (91.77%)
+    car2.jpg - Prediction: beach_wagon (54.96%)
+    cat.jpg - Prediction: Egyptian_cat (34.26%)
+    plane.jpg - Prediction: airliner (75.09%)
